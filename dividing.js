0f7eb8e1b6c03d9495730dbb42d5ca5909756475
Cihan T. <cihan@yucin.com>
2015-07-11 13:20:27 +0300

    Update dividing.js

--- dividing.js
+++ dividing.js
@@ -1,15 +1,17 @@
 var r = [];
 
-while(r.length < 10)
+while(r.length < 1000)
 {
-	var a = Math.floor((Math.random() * 1000) + 1);
+	var a = getRandomInt(100,1000);
     
-    if(a > 99){
-    	var b = Math.floor((Math.random() * 100) + 1);
-        
-        if(b > 9 && a % b == 0){
+    	var b = getRandomInt(10,100)
+        var key = a+','+b;
+        if(a % b == 0 && r.indexOf(key)){
         	document.write(r.length +1, ')  ', a, ' / ', b, ' = ', a/b, '<br/>');
-            r.push(a);
+        	r.push(key);
         }
-    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
